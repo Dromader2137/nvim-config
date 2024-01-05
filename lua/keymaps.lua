c4e3200b3a97fbd53974e442384351b0512aa20b
Dromader2137 <wojciechowski.ignacy@gmail.com>
2024-01-05 16:23:16 +0100

    asdasdasad

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>e', ':Ex<CR>')
-vim.keymap.set('n', '<leader>t', ':TroubleToggle<CR>')
+vim.keymap.set('n', '<leader>t', ':! ./test.sh<CR>')
+vim.keymap.set('n', '<leader>d', ':TroubleToggle<CR>')
