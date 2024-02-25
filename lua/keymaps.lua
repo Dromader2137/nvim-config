2bd79e41e3c2b0a8fc2af53dfdbeb85889920d70
Dromader2137 <wojciechowski.ignacy@gmail.com>
2024-02-25 19:27:51 +0100

    Update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<leader>e', ':Ex<CR>')
 vim.keymap.set('n', '<leader>t', ':! ./test.sh<CR>')
+vim.keymap.set('n', '<leader>b', ':! ./build.sh<CR>')
 vim.keymap.set('n', '<leader>d', ':TroubleToggle<CR>')
--- lua/plugin_config/telescope.lua
+++ lua/plugin_config/telescope.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
 vim.keymap.set('n', '<leader>fg', ':Telescope git_files<CR>')
+vim.keymap.set('n', '<leader>fr', ':Telescope live_grep<CR>')
